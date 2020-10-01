4a1e45ec6bde39986c379138bb246de1df89a3c3
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-01 12:23:20 +0200

    Conecting api to client, in index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'semantic-ui-css/semantic.min.css'
 import * as serviceWorker from './serviceWorker';
+import axios from "axios";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const App = () => {
+
+}
+
+let apiUrl;
+
+if (process.env.NODE_ENV === "production" {
+  apiUrl = 'https://api.github.com/user/repos';
+}
+else
+{
+  apiUrl = "http://localhost:3000/api/v3";
+}
+axios.defaults.baseURL = apiUrl;
+
+ReactDOM.render(<App />, document.getElementById( "root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
