9c559368d03517f0e8eb7e9b2d8b6c530ffaa9f5
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-10-22 22:00:47 +0200

    returning mat function

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,30 +1,30 @@
-import React, { useState, useEffect} from 'react'
-import { Button, Input } from 'semantic-ui-react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { Button, Input } from "semantic-ui-react";
+import axios from "axios";
 
 const GHSearch = () => {
+  const [search, setSearch] = useState([]);
 
-const [search, setSearch] = useState([])
+  useEffect(() => {
+    fetchName();
+  }, []);
 
+  const fetchName = async () => {
+    let response = await axios.get();
+  };
 
+  let itemDisplay = item.map((search) => {
+    return (
 
-useEffect(() => {
-  fetchName();
-}, []);
+      <>
+      <li>
+        {item.login}
+      </li>
+        <Input type="text" name="search" placeholder="Input GH username" />
+        <Button name="search">Search</Button>
+      </>
+    );
+  });
+};
 
-const fetchName = async () => {
-  let response = await axios.get(
-
-  )
-}
-
-
-  return (
-    <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
-    </>
-  )
-}
-
-export default GHSearch
+export default GHSearch;
